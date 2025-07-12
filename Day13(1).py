1f5d0d47cf4bce92637152c8e692e5583466aa51
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-07-12 20:30:56 +0530

    Is Anagram (Case-Insensitive)

--- Day13(1).py
+++ Day13(1).py
@@ -1,2 +1,17 @@
-print("Hi git")
-print("changes")
+# # Write a function is_anagram(s1, s2) 
+# # that checks if two strings are anagrams (case-insensitive and ignoring spaces).
+
+# # 📌 An anagram is a rearrangement of letters forming another word.
+
+# Input: "Listen", "Silent"
+# Output: True
+
+# Input: "School master", "The classroom"
+# Output: True
+
+def isAnagram(s1,s2):
+    s1 = s1.replace(" ","").lower()
+    s2 = s2.replace(" ","").lower()
+    return sorted(s1) == sorted(s2)
+
+print(isAnagram("Listen", "Silent"))

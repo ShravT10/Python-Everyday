7e04cf0cdc835258d6e3c8d81870be8da041af69
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-07-24 19:44:54 +0530

    Transpose a Matrix

--- Day13(2).py
+++ Day13(2).py
@@ -1,1 +1,14 @@
-#WAssaappapappapapag
+# #Write a function transpose(matrix) that returns the transpose of a 2D list.
+# 📌 A transpose flips rows into columns.
+
+# Input: [[1, 2, 3],
+#         [4, 5, 6]]
+
+# Output: [[1, 4],
+#          [2, 5],
+#          [3, 6]]
+
+def tranpose(matrix):
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    
+print(tranpose([[1, 2, 3],[4, 5, 6]]))

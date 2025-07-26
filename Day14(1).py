295f507f27e9ae92a8e688bae5ce85aecd2cae09
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-07-26 16:54:05 +0530

    Sliding Window

--- Day14(1).py
+++ Day14(1).py
@@ -1,1 +1,16 @@
-hi
+# # Type: Sliding Window (Popular in Interviews)
+# # Write a function max_sum_subarray(arr, k)
+# # that returns the maximum sum of any contiguous subarray of size k.
+
+# Input: arr = [2, 1, 5, 1, 3, 2], k = 3
+# Output: 9  (subarray: [5, 1, 3])
+
+def max_sum_subarray(arr, k):
+    max_sum = 0
+    for i in range(0, len(arr) - k + 1):
+        current_sum = sum(arr[i:i+k])  # compute subarray sum
+        if current_sum > max_sum:
+            max_sum = current_sum
+    return max_sum
+
+print(max_sum_subarray([2, 1, 5, 1, 3, 2], 3))  # Output: 9

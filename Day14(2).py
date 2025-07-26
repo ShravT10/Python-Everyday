e584e341726232184a261b2d9115feadac8c5f3a
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-07-26 17:00:14 +0530

    Palindrome String

--- Day14(2).py
+++ Day14(2).py
@@ -1,1 +1,12 @@
-hi
+# # Write a function is_palindrome(s) 
+# # that returns True if the string is a palindrome, ignoring case and spaces
+
+# Input: "A man a plan a canal Panama"
+# Output: True
+
+
+def is_palindrome(s):
+    res = ''.join(c.lower() for c in s if c.isalnum())
+    return res == res[::-1]
+
+print(is_palindrome("A man a plan a canal Panama"))

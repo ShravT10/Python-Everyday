dcfb0510dbc7cbc34b99011b128a30238b541778
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-07-28 19:23:13 +0530

    Rotate a Matrix

--- Day14(3).py
+++ Day14(3).py
@@ -1,1 +1,30 @@
-hi
+# # Write a function rotate_matrix(matrix) 
+# # that rotates a square matrix 90° clockwise.
+
+# Input:
+# [
+#  [1, 2, 3],
+#  [4, 5, 6],
+#  [7, 8, 9]
+# ]
+
+# Output:
+# [
+#  [7, 4, 1],[0][0] -> [0][2]
+#  [8, 5, 2],[1][0] -> [0][1]
+#  [9, 6, 3] [2][0] -> [0][0]
+# ]
+
+def rotate_matrix(matrix):
+    return [list(row) for row in zip(*matrix[::-1])]
+
+matrix = [
+ [1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9]
+]
+
+rotated = rotate_matrix(matrix)
+
+for row in rotated:
+    print(row)

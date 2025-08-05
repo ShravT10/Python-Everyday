db258fb3b1851142cbcc5b8e4fdf0512cf0d3345
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-08-05 20:24:46 +0530

    Check proper closing of brackets in a string

--- Day15(2).py
+++ Day15(2).py
@@ -1,1 +1,32 @@
-hi
+# # Difficulty: Easy
+# # Description:
+# # Given a string s containing only ()[]{},
+# # determine if the input string is valid. A string is valid if:
+
+# # Open brackets are closed by the same type of brackets.
+
+# # Open brackets are closed in the correct order.
+
+# Input: s = "()[]{}"
+# Output: True
+
+# Input: s = "(]"
+# Output: False
+
+def check_valid(s):
+    ref = {"(":0,
+           ")":0,
+           "[":0,
+           "]":0,
+           "{":0,
+           "}":0}
+    
+    for i in s:
+        ref[i] = ref.get(i,0) + 1
+    
+    if ref["("] == ref[")"] and ref["["] == ref["]"] and ref["{"] == ref["}"]:
+        return "Valid string"
+    else:
+        return "Invalid string"
+    
+print(check_valid("(){}[]"))

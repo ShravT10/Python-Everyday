6cc6d8aed06aa6785c031d05ae05a0f313774c8c
ShravT10 <160204774+ShravT10@users.noreply.github.com>
2025-08-05 20:32:34 +0530

    move zeros to end of a list

--- Day15(3).py
+++ Day15(3).py
@@ -1,1 +1,20 @@
-hi
+# Given an array nums, write a 
+# function to move all 0's to the end while maintaining the relative order of the non-zero elements.
+
+# You must do this in-place without making a copy of the array.
+
+# Input: nums = [0, 1, 0, 3, 12]
+# Output: [1, 3, 12, 0, 0]
+
+
+def move_zero(lst1):
+    lst = lst1
+    for i in range(len(lst)):
+        if lst[i] == 0:
+            lst.pop(i)
+            lst.append(0)
+    return lst
+
+print(move_zero([0, 1, 0, 3, 12]))
+
+
